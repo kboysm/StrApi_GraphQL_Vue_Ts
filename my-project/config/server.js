b5d137701ea33c68a6e0f8289c56142e9ff6cb85
kboysm <lishal12@naver.com>
2020-08-08 21:08:24 +0900

    strapi+graphql test 및 graphql playground 활용

---
+++ my-project/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '54eb7438370cde626f1533d3ea4aeda8'),
+    },
+  },
+});
---
+++ my-project/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e172e5fe-152f-482a-894d-111c98746742'
+};
